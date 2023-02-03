ebd44c2b6170635075d90d2b9a90bd1c4e7df6c1
Vika Shcherbak <vikashcherbaka@gmail.com>
2023-02-03 15:20:12 +0100

    quotes added &&  shuffled, 2Darr & CSS greed created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import quotes from './quotes.json';
 
 function App() {
+
+  const [board, setBoard] = useState(null);
+
+  useEffect(() => {
+    let twoDBoard = [];
+    let initBingo = quotes.sort(() => Math.random() - 0.5).slice(0,24).map(quote => {
+      return { quote: quote, isClicked: false}
+    });
+    initBingo.splice(12, 0, { quote: "CONF CALL BINGO", isClicked: true}); //shuffle & insert middle one
+    for (let i = 0; i < initBingo.length; i += 5) { //make 2D array
+			twoDBoard.push(initBingo.slice(i, i + 5));
+		}
+    setBoard(twoDBoard);
+    console.log(twoDBoard);
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container text-center grid'>
+      <div className='row row-cols-5'>
+      {board ? board.flatMap(function (innerArray, row) {
+				return innerArray.map(function (element, col) {
+					return (
+          <button key={''+row+col}>{row}{col} {element.quote}</button>)
+				})
+			}): null}
+      </div>
     </div>
   );
 }
